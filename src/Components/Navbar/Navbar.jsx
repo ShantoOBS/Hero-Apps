b9fac1117187ab582b75f193b733b991c81f7f80
ShantoOBS <roys22127@gmail.com>
2025-10-08 23:53:57 +0600

    Add errors pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,60 +5,13 @@ import { IoLogoGithub } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
 import icon from '../../assets/logo.png'
 
-import { IoHomeOutline } from "react-icons/io5";
 
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
 
-
-    return (
-        <div className="shadow-sm">
-
-            <div className='max-w-6xl mx-auto navbar'>
-
-
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <RxHamburgerMenu />
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content rounded-b-box z-1 mt-3 w-100 p-2 shadow bg-white">
-
-                            <NavLink
-                                to="/"
-                                className={({ isActive }) =>
-                                    isActive
-                                        ? "text-green-500 font-bold border-b-2 border-green-500"
-                                        : "text-gray-600"
-                                }
-                            >
-                                Home
-                            </NavLink>
-
-
-                            <Link>Apps</Link>
-                            <Link>Installation</Link>
-
-                        </ul>
-                    </div>
-                    <Link
-                        to="/"
-                        className="flex items-center justify-center gap-2"
-                    >
-                        <img src={icon} alt="Hero Logo" className="w-8 h-8" />
-                        <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-transparent bg-clip-text font-bold">
-                            HERO.IO
-                        </span>
-                    </Link>
-
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 flex gap-4 font-bold">
-
-                        <NavLink
+     const links = <>
+               <NavLink
                             to="/"
                             className={({ isActive }) =>
                                 isActive
@@ -67,9 +20,7 @@ export default function Navbar() {
                             }
                         >
 
-                            <div className='flex gap-2 items-center justify-center '>
-                                <IoHomeOutline />  Home
-                            </div>
+                         Home
 
                         </NavLink>
 
@@ -95,6 +46,47 @@ export default function Navbar() {
                         >
                             Installation
                         </NavLink>
+      </>
+
+
+    return (
+        <div className="shadow-sm">
+
+            <div className='max-w-6xl mx-auto navbar'>
+
+
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <RxHamburgerMenu />
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content rounded-b-box z-1 mt-3 w-100 p-2 shadow bg-white">
+
+                          {
+                            links
+                          }
+
+                        </ul>
+                    </div>
+                    <Link
+                        to="/"
+                        className="flex items-center justify-center gap-2"
+                    >
+                        <img src={icon} alt="Hero Logo" className="w-8 h-8" />
+                        <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-transparent bg-clip-text font-bold">
+                            HERO.IO
+                        </span>
+                    </Link>
+
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 flex gap-4 font-bold">
+
+                      {
+                        links
+                      }
 
 
                     </ul>
--- src/Pages/ErrorElement/ErrorElement.jsx
+++ src/Pages/ErrorElement/ErrorElement.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
+import Error from '../../Components/Error/Error'
 
 export default function ErrorElement() {
   return (
-    <div>
-         <p>error element</p>
-    </div>
+    <div className=' text-black bg-[#f7f2f2]'>
+   
+           <div className='bg-white'><Navbar></Navbar></div>
+           
+           
+           <Error></Error>
+       
+           
+           <Footer></Footer>
+       </div>
   )
 }
